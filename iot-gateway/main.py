eec3fd2b24ff05ad095ae41cfd8fdcb55c1dbf22
Nicolo Lüscher <nicolo.luescher@students.bfh.ch>
2022-10-28 15:59:20 +0200

    updated main.py

--- iot-gateway/main.py
+++ iot-gateway/main.py
@@ -1,45 +1,9 @@
-#!/usr/local/bin/python3
-
-# Elia Bieri
-
-import time
-
-from rich.live import Live
-from rich.table import Table
-
-from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
-
-mystrom_devices : list[str] = []
-
-class MyListener(ServiceListener):
-
-    def update_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        pass
-
-    def remove_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        pass
-
-    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
-        info = zc.get_service_info(type_, name)
-        mystrom_devices.append(info.server[:-1])
-
-def generate_table() -> Table:
-    table = Table()
-    table.add_column("name")
-
-    for device in mystrom_devices:
-        table.add_row(device)
-    return table
-
-
 def main():
-    zeroconf = Zeroconf()
-    listener = MyListener()
-    browser = ServiceBrowser(zeroconf, "_hap._tcp.local.", listener)
-    with Live(generate_table(), refresh_per_second=4) as live:
-        for _ in range(40):
-            time.sleep(0.4)
-            live.update(generate_table())
+    scanner = Scanner()
+    print("waiting 10 seconds")
+    time.sleep(10)
+    strom = myStromManagement(scanner.get_device_hostnames())
+    
 
-if __name__ == "__main__":
+if __name__ =="__main__":
     main()
