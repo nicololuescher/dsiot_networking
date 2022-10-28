cb9b1e5e0ae64ceaa2780840c47e31c750dbbc3b
Nicolo Lüscher <nicolo.luescher@students.bfh.ch>
2022-10-28 16:04:44 +0200

    fixed main.py

--- iot-gateway/main.py
+++ iot-gateway/main.py
@@ -1,7 +1,12 @@
+from myStromManagement import myStromManagement
+from scanner import Scanner
+import time
+
 def main():
     scanner = Scanner()
     print("waiting 10 seconds")
     time.sleep(10)
+    print("found devices: " + str(scanner.get_device_hostnames()))
     strom = myStromManagement(scanner.get_device_hostnames())
     
 
